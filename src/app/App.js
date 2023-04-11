c47f20012b2b63340e5fb10c248cc712407d0cf6
aarkuo317 <c66578812@gmail.com>
2023-04-11 16:31:43 +0800

    dddd

---
+++ src/app/App.js
@@ -0,0 +1,60 @@
+import React from "react";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+import About from "../components/About";
+import SignUp from "../components/SignUp";
+import Articles from "../components/Articles";
+import Article from "../components/Article";
+import Categories from "../components/Categories";
+import Author from "../components/Author";
+import Profile from "../components/Profile";
+
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+import "./App.css";
+
+
+
+function App() {
+  return (
+    <Router>
+      <Header />
+      <main>
+        <Switch>
+          <Route path='/about'>
+            <About />
+          </Route>
+
+          <Route path='/sign-up'>
+            <SignUp />
+          </Route>
+
+          {/* switch可以把類似的東西包再一起決定先後順序，以免一起被渲染 */}
+
+          <Route path='/articles/:title'>
+            <Article />
+          </Route>
+          <Route path='/articles/'>
+            <Articles />
+          </Route>
+
+
+
+          <Route path='/authors/:name'>
+            <Author />
+          </Route>
+          <Route path='/categories'>
+            <Categories />
+          </Route>
+
+          <Route path='/profile'>
+            <Profile />
+          </Route>
+        </Switch>
+      </main>
+      <Footer />
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/components/Categories.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { selectCategories } from '../features/categories/categoriesSlice';
+import { Route, Link, useRouteMatch,} from 'react-router-dom';
+import Category from './Category'
+
+export default function Categories () {
+  const categories = useSelector(selectCategories)
+  const { path, url, } = useRouteMatch()
+  // const {name} = useParams()
+  console.log(path, url)
+  return (
+    <main>
+
+      <h1>Categories</h1>
+      <ul>
+        { 
+          Object.keys(categories).map(category => {
+            return (
+              <li key={category}>
+                <Link to={`${url}/${category}`}>{category}</Link>
+              </li>
+            )
+          })
+        }
+      </ul>
+      <Route path={`${path}/:name`}>
+        <Category />
+      </Route>
+    </main>
+  )
+}
